3834f6749a8fce886f9cb1bc9c73dc4d777f9a75
Khue Dao <73230749+KhueDao29@users.noreply.github.com>
2023-05-22 16:40:33 +0700

    Add files via upload

--- post_page.dart
+++ post_page.dart
@@ -1,19 +1,32 @@
 /// Main coder: Khue
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:Fulbright_Vietnam_Forum/NaviBar.dart';
 import 'package:like_button/like_button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-/// Change to Theme.of(context).colorScheme.primary
-const fulbrightBlue = Color(0xFF00196E);
+//Hard coding Category and Topic
+const Category = "Student Life";
+const Topic = "Baking";
 
-/// Change to Theme.of(context).colorScheme.secondary
-const fulbrightGold = Color(0xFFFFAD1D);
+// Query data //
+var db = FirebaseFirestore.instance;
+final data = db
+    .collection('post')
+    .where('Category', isEqualTo: 'Student Life')
+    .where('Topic', isEqualTo: 'Baking')
+    .get()
+    .then((querySnapshot) {
+  return [for (var docSnapshot in querySnapshot.docs) docSnapshot.data()];
+});
 
-List<String> contentData = [
-  "Hi everyone,\n\nI'm curious to know what are some of the best studying methods that you use to prepare for exams. Studying methods are ways to help you remember important concepts and information. Some studying methods that I have heard of are:\n-  Spaced repetition: This involves separating your study sessions into spaced intervals, such as reviewing the material every few days until you memorize it.\n-  Active recall: This involves testing yourself on the material without looking at the answers, such as using flashcards or practice questions.\n-  Feynman technique: This involves explaining the material in your own words as if you were teaching it to someone else, such as a friend or a child.\n-  SQ3R: This stands for survey, question, read, recite, and review. It is a reading comprehension technique that helps you identify important facts and retain information from your textbook.\n\nWhat do you think of these methods? Do you use any of them? Do you have any other methods that work well for you? Please share your thoughts and experiences in the comments below. I look forward to hearing from you!",
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  "Cursus metus aliquam eleifend mi. In metus vulputate eu scelerisque felis imperdiet. Amet consectetur adipiscing elit ut aliquam purus. Curabitur vitae nunc sed velit dignissim sodales ut. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Orci sagittis eu volutpat odio facilisis. Egestas integer eget aliquet nibh. Sit amet commodo nulla facilisi nullam vehicula ipsum a. Est ultricies integer quis auctor elit sed vulputate mi sit. Feugiat pretium nibh ipsum consequat nisl vel. Mus mauris vitae ultricies leo integer malesuada nunc vel. Dolor sed viverra ipsum nunc. Lacus vestibulum sed arcu non odio euismod lacinia. Iaculis nunc sed augue lacus. Lacinia quis vel eros donec ac odio tempor. Scelerisque varius morbi enim nunc faucibus a."
-]; //hard coded
+// List<String> contentData = [
+//   "Hi everyone,\n\nI'm curious to know what are some of the best studying methods that you use to prepare for exams. Studying methods are ways to help you remember important concepts and information. Some studying methods that I have heard of are:\n-  Spaced repetition: This involves separating your study sessions into spaced intervals, such as reviewing the material every few days until you memorize it.\n-  Active recall: This involves testing yourself on the material without looking at the answers, such as using flashcards or practice questions.\n-  Feynman technique: This involves explaining the material in your own words as if you were teaching it to someone else, such as a friend or a child.\n-  SQ3R: This stands for survey, question, read, recite, and review. It is a reading comprehension technique that helps you identify important facts and retain information from your textbook.\n\nWhat do you think of these methods? Do you use any of them? Do you have any other methods that work well for you? Please share your thoughts and experiences in the comments below. I look forward to hearing from you!",
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//   "Cursus metus aliquam eleifend mi. In metus vulputate eu scelerisque felis imperdiet. Amet consectetur adipiscing elit ut aliquam purus. Curabitur vitae nunc sed velit dignissim sodales ut. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Orci sagittis eu volutpat odio facilisis. Egestas integer eget aliquet nibh. Sit amet commodo nulla facilisi nullam vehicula ipsum a. Est ultricies integer quis auctor elit sed vulputate mi sit. Feugiat pretium nibh ipsum consequat nisl vel. Mus mauris vitae ultricies leo integer malesuada nunc vel. Dolor sed viverra ipsum nunc. Lacus vestibulum sed arcu non odio euismod lacinia. Iaculis nunc sed augue lacus. Lacinia quis vel eros donec ac odio tempor. Scelerisque varius morbi enim nunc faucibus a."
+// ]; //hard coded
+
+List<String> contentData = [];
 
 List<String> captionData = [
   "How do you study effectively for exams?",
@@ -39,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _postAuthorRow(BuildContext context) {
+  Widget _postAuthorRow(BuildContext context, String user) {
     const double avatarDiameter = 30;
     return GestureDetector(
       child: Row(
@@ -62,9 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          const Text(
-            'Username',
-            style: TextStyle(
+          Text(
+            user,
+            style: const TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 10,
             ),
@@ -74,35 +87,35 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _postCaption(int index) {
-    String caption = captionData[index];
+  Widget _postCaption(String content) {
+    String caption = content;
     return Padding(
-      padding: EdgeInsets.symmetric(
+      padding: const EdgeInsets.symmetric(
         horizontal: 8,
         vertical: 4,
       ),
-      child: Text(caption, style: TextStyle(fontSize: 16)),
+      child: Text(caption, style: const TextStyle(fontSize: 16)),
     );
   }
 
-  Widget _postContent(int index) {
+  Widget _postContent(String content) {
     String subText;
-    if (contentData[index].length < 101) {
-      subText = contentData[index];
+    if (content.length < 250) {
+      subText = content;
     } else {
-      subText = contentData[index].substring(0, 100);
+      subText = content.substring(0, 250) + "...";
     }
     ;
     return Padding(
-        padding: EdgeInsets.symmetric(
+        padding: const EdgeInsets.symmetric(
           horizontal: 8,
           vertical: 4,
         ),
         //text should be imported from database and not hardcode
-        child: Text("$subText...", style: TextStyle(fontSize: 12)));
+        child: Text(subText, style: const TextStyle(fontSize: 12)));
   }
 
-  Widget _postImage() {
+  Widget _postImage(String image) {
     return Center(
         child: Container(
       width: 400,
@@ -110,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
       child: AspectRatio(
         aspectRatio: 16 / 9,
         child: Image.asset(
-          'images/topicpage/inclusivity.png',
+          image,
           fit: BoxFit.cover,
         ),
       ),
@@ -130,12 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget interactButtons(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-      // _likeCount(context),
-      // _favoriteButton(context),
-      // FavoriteButton(
-      //   valueChanged: (_) {},
-      // ),
-      LikeButton(
+      const LikeButton(
         size: 27,
         likeCount: 635,
         //onTap: onLikeButtonTapped, #send to the server
@@ -178,22 +186,22 @@ class _MyHomePageState extends State<MyHomePage> {
                             });
                             Navigator.pop(context);
                           },
-                          child: Text('edit'))
+                          child: const Text('edit'))
                     ],
                   ));
         },
         icon: const Icon(Icons.edit));
   }
 
-  Widget _postView(BuildContext context, int index) {
+  Widget _postView(BuildContext context, Map<String, dynamic> post, int index) {
     return ListTile(
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _postAuthorRow(context),
-            _postImage(),
-            _postCaption(index),
-            _postContent(index),
+            _postAuthorRow(context, post['user'].toString()),
+            _postImage(post['Image'].toString()),
+            _postCaption(post['Title'].toString()),
+            _postContent(post['Content'].toString()),
             interactButtons(context),
           ],
         ),
@@ -208,34 +216,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _postsListView(BuildContext context) {
-    return Scaffold(
-        body: ListView.builder(
-            itemCount: contentData.length,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: const EdgeInsets.all(15.5),
-                padding: const EdgeInsets.all(5.0),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 3,
-                    color: Colors.black,
+    return FutureBuilder(
+        future: data,
+        builder: (context, snapshot) {
+          List<Map<String, dynamic>>? posts;
+          if (snapshot.hasData) {
+            posts = snapshot.data;
+          } else {
+            // ignore: avoid_print
+            print("Cannot query data");
+            posts = [];
+          }
+          return ListView.builder(
+              itemCount: posts!.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.all(15.5),
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 3,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                //text imported from database
-                child: _postView(context, index),
-              );
-            }),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("prompt new post page"); //!TO DO:prompt post page to open
-          },
-          backgroundColor: fulbrightBlue,
-          mouseCursor: SystemMouseCursors.click,
-          tooltip: "Create a new post",
-          child: const Icon(
-            Icons.add_rounded,
-            size: 60,
-          ),
-        ));
+                  //text imported from database
+                  child: _postView(context, posts![index], index),
+                );
+              });
+        });
   }
 }
