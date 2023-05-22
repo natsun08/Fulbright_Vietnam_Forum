527a63158b7019263240ef7fa004ff5e2ff5afc7
Khue Dao <73230749+KhueDao29@users.noreply.github.com>
2023-05-22 16:50:35 +0700

    post_page backend: username, content, caption, like count
    - can't delete post -> need to rewrite database
    - can't edit post -> see above
    - like count is not updated when pressed -> see above
    - can't fetch user profile pic yet -> need to import users collection

--- post_page.dart
+++ post_page.dart
@@ -20,20 +20,6 @@ final data = db
   return [for (var docSnapshot in querySnapshot.docs) docSnapshot.data()];
 });
 
-// List<String> contentData = [
-//   "Hi everyone,\n\nI'm curious to know what are some of the best studying methods that you use to prepare for exams. Studying methods are ways to help you remember important concepts and information. Some studying methods that I have heard of are:\n-  Spaced repetition: This involves separating your study sessions into spaced intervals, such as reviewing the material every few days until you memorize it.\n-  Active recall: This involves testing yourself on the material without looking at the answers, such as using flashcards or practice questions.\n-  Feynman technique: This involves explaining the material in your own words as if you were teaching it to someone else, such as a friend or a child.\n-  SQ3R: This stands for survey, question, read, recite, and review. It is a reading comprehension technique that helps you identify important facts and retain information from your textbook.\n\nWhat do you think of these methods? Do you use any of them? Do you have any other methods that work well for you? Please share your thoughts and experiences in the comments below. I look forward to hearing from you!",
-//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//   "Cursus metus aliquam eleifend mi. In metus vulputate eu scelerisque felis imperdiet. Amet consectetur adipiscing elit ut aliquam purus. Curabitur vitae nunc sed velit dignissim sodales ut. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Orci sagittis eu volutpat odio facilisis. Egestas integer eget aliquet nibh. Sit amet commodo nulla facilisi nullam vehicula ipsum a. Est ultricies integer quis auctor elit sed vulputate mi sit. Feugiat pretium nibh ipsum consequat nisl vel. Mus mauris vitae ultricies leo integer malesuada nunc vel. Dolor sed viverra ipsum nunc. Lacus vestibulum sed arcu non odio euismod lacinia. Iaculis nunc sed augue lacus. Lacinia quis vel eros donec ac odio tempor. Scelerisque varius morbi enim nunc faucibus a."
-// ]; //hard coded
-
-List<String> contentData = [];
-
-List<String> captionData = [
-  "How do you study effectively for exams?",
-  "Caption 2",
-  "Caption 3"
-]; //hard coded
-
 String text = '';
 
 class MyHomePage extends StatefulWidget {
@@ -141,11 +127,11 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget interactButtons(BuildContext context) {
+  Widget interactButtons(BuildContext context, int likeCount) {
     return Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-      const LikeButton(
+      LikeButton(
         size: 27,
-        likeCount: 635,
+        likeCount: likeCount,
         //onTap: onLikeButtonTapped, #send to the server
       ),
       _commentButton(context),
@@ -202,7 +188,7 @@ class _MyHomePageState extends State<MyHomePage> {
             _postImage(post['Image'].toString()),
             _postCaption(post['Title'].toString()),
             _postContent(post['Content'].toString()),
-            interactButtons(context),
+            interactButtons(context, post['like_count']),
           ],
         ),
         trailing: Container(
